b943ccb23106cc35a820c6a38b95093c5c4e7c6c
Nikita Tsapko <otghikogh@ya.ru>
2024-04-21 22:09:41 +0300

    developing

--- core/template/files/upload.php
+++ core/template/files/upload.php
@@ -7,22 +7,24 @@
 <body>
 <?php
 if($_FILES) {
-    foreach ($_FILES["uploads"]["error"] as $key => $error) {
-        if(count($_FILES["uploads"]["name"]) > 10) {
-            exit("Количество файлов не должно превышать 10.");
-        }
-        if ($error == UPLOAD_ERR_OK) {
-            $tmp_name = $_FILES["uploads"]["tmp_name"][$key];
-            $name = $_FILES["uploads"]["name"][$key];
-            if($_FILES["uploads"]["size"][$key] > 52428800) {
-                echo "Ошибка! Вес файла ". $_FILES["uploads"]["name"][$key] ." не должен превышать 50 МБ";
-                continue;
-            }
-            move_uploaded_file($tmp_name, "../../brigada-miit-storage/".$name);
-        }
+    if(count($_FILES["uploads"]["name"]) > 10) {
+        echo "Количество файлов не должно превышать 10.";
     }
-    echo "Файлы загружены";
-    print_r($_FILES);
+    else {
+        foreach ($_FILES["uploads"]["error"] as $key => $error) {
+            if ($error == UPLOAD_ERR_OK) {
+                $tmp_name = $_FILES["uploads"]["tmp_name"][$key];
+                $name = $_FILES["uploads"]["name"][$key];
+                if($_FILES["uploads"]["size"][$key] > 52428800) {
+                    echo "Ошибка! Вес файла ". $_FILES["uploads"]["name"][$key] ." не должен превышать 50 МБ";
+                    continue;
+                }
+                move_uploaded_file($tmp_name, "../../brigada-miit-storage/".$name);
+            }
+        }
+        echo "Файлы загружены";
+        print_r($_FILES);
+    }
 }
 ?>
 <h2>Загрузка файла</h2>
