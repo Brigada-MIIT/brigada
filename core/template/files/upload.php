ab2cf85e3c47e69b17102efa21b6a9e777bed217
Nikita Tsapko <otghikogh@ya.ru>
2024-04-22 00:05:48 +0300

    test fix

--- core/template/files/upload.php
+++ core/template/files/upload.php
@@ -121,8 +121,8 @@ body {
 				'formData'         : {
 									   'timestamp'  : '<?php echo $timestamp;?>',
 									   'token'      : '<?php echo md5('unique_salt' . $timestamp);?>',
-                                       'name'       : document.getElementById("name").value,
-                                       'description': document.getElementById("description").value,
+                                       'name'       : `${document.getElementById("name").value}`,
+                                       'description': `${document.getElementById("description").value}`,
 				                     },
 				'queueID'          : 'queue',
 				'uploadScript'     : '/api/files/upload',
