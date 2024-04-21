3e43b999166bb5a896ccd115d9a538e2dd1eb3eb
Nikita Tsapko <otghikogh@ya.ru>
2024-04-21 22:00:16 +0300

    developing

--- core/template/files/upload.php
+++ core/template/files/upload.php
@@ -8,9 +8,17 @@
 <?php
 if($_FILES) {
     foreach ($_FILES["uploads"]["error"] as $key => $error) {
+        if(count($_FILES["uploads"]) > 10) {
+            echo "Количество файлов не должно превышать 10.";
+            exit(-1);
+        }
         if ($error == UPLOAD_ERR_OK) {
             $tmp_name = $_FILES["uploads"]["tmp_name"][$key];
             $name = $_FILES["uploads"]["name"][$key];
+            if($_FILES["uploads"]["size"][$key] > 52428800) {
+                echo "Ошибка! Вес файла ". $_FILES["uploads"]["name"][$key] ." не должен превышать 50 МБ";
+                continue;
+            }
             move_uploaded_file($tmp_name, "../../brigada-miit-storage/".$name);
         }
     }
