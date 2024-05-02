a740db366b75b4cea7d211a30f670d83a34d3346
Nikita <otghikogh@ya.ru>
2024-05-02 11:16:19 +0300

    developing

--- core/template/default.php
+++ core/template/default.php
@@ -34,6 +34,16 @@
                     <li class="nav-item">
                         <a class="nav-link" href="{{ url('/register') }}">Регистрация</a>
                     </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="bi bi-person-circle"></i> nickname
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </li>
                 </ul>
             </div>
         </nav>
