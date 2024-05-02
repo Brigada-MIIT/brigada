bf74f4fb91d4fbeb6ea197129e5c972f4a4dda1e
Nikita <otghikogh@ya.ru>
2024-05-02 11:09:31 +0300

    developing

--- core/template/default.php
+++ core/template/default.php
@@ -18,7 +18,7 @@
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="navbar-header" id="navbarSupportedContent">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
                         <a class="nav-link" href="#">Главная <span class="sr-only">(current)</span></a>
@@ -41,17 +41,11 @@
                         <a class="nav-link disabled" href="#">Disabled</a>
                     </li>
                 </ul>
+                <a class="nav-link">Test</a>
                 <form class="form-inline my-2 my-lg-0">
                     <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
                     <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                 </form>
-            </div>
-            <div class="navbar-collapse collapse text-right" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link">Link<span class="sr-only">(current)</span></a>
-                    </li>
-                </ul>
             </div>
         </nav>
     </header>
