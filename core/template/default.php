4d982ac324fd4f00a18984ace4cb3941a24d8556
Nikita <otghikogh@ya.ru>
2024-05-02 11:14:40 +0300

    developing

--- core/template/default.php
+++ core/template/default.php
@@ -12,7 +12,9 @@
   <body>
     <header>
         <nav class="navbar navbar-expand-md navbar-light bg-light">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="#">
+                <img src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="">
+             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
