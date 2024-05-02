1d6263069fa5516b06f87014846e5b2c624b2221
Nikita <otghikogh@ya.ru>
2024-05-02 10:46:56 +0300

    developing

--- core/template/default.php
+++ core/template/default.php
@@ -1,43 +1,15 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Test</title>
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <link rel="stylesheet" href="/assets/css/dashboard.css?ver=2">
-        <meta name="viewport" content="initial-scale=1, width=device-width, maximum-scale=1, minimum-scale=1, user-scalable=no">
-        <script src="/assets/js/jquery.min.js"></script>
-        <link rel="stylesheet" type="text/css" href="/assets/css/jquery.datetimepicker.css">
-        <script src="/assets/js/jquery.datetimepicker.full.min.js"></script>
-        <script src="/assets/js/sweetalert2.js"></script>
-        <script src="https://www.kryogenix.org/code/browser/sorttable/sorttable.js"></script>
-        <!--<link rel="stylesheet" href="/assets/css/main.css">-->
-        <link rel="SHORTCUT ICON" href="/assets/img/logo_logs_notepad.ico" type="image/x-icon">
-    </head>
-    <body>
-        <div class="wrapper">
-        <header>
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-12">
-
-                    </div>
-                </div>
-            </div>
-        </header>
-        <main>
-            <?php include $content;?>
-        </main>
-        <footer>
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-12">
-                        <b>По тех. вопросам — <a href="https://vk.com/test">test</a>.</b>
-                    </div>
-                </div>
-            </div>
-        </footer>
-        </div>
-    </body>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
 </html>
