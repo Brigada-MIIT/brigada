6d32905b42ab2e9112fc032183de1d000d4506de
Nikita <otghikogh@ya.ru>
2024-05-02 11:31:53 +0300

    developing

--- core/template/default.php
+++ core/template/default.php
@@ -28,23 +28,25 @@
                     </li>
                 </ul>
                 <ul class="navbar-nav">
+                    <?php if(!$system->auth()) echo `
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ url('/login') }}">Логин</a>
+                        <a class="nav-link" href="/app/auth">Логин</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ url('/register') }}">Регистрация</a>
-                    </li>
+                        <a class="nav-link" href="/app/register">Регистрация</a>
+                    </li>`;
+                    else echo `
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16" style="top: 0.13em;position: rela><path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/><path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/></svg>
-                            nickname
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16" style="top: 0.13em;position: relative"><path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/><path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/></svg>
+                            `. $_user['surname'] .`
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink" style="left: -90%;">
                             <a class="dropdown-item" href="#">Action</a>
                             <a class="dropdown-item" href="#">Another action</a>
                             <a class="dropdown-item" href="#">Something else here</a>
                         </div>
-                    </li>
+                    </li>`; ?>
                 </ul>
             </div>
         </nav>
