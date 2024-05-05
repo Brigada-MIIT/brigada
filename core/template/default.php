fd91470d946d5ac3ba2f5813e15fa94b14c18122
Nikita Tsapko <otghikogh@ya.ru>
2024-05-05 18:29:33 +0300

    developing

--- core/template/default.php
+++ core/template/default.php
@@ -20,7 +20,7 @@
         <header>
             <nav class="navbar navbar-expand-md navbar-light bg-light">
                 <a class="navbar-brand" href="#">
-                    <img src="/assets/img/logo_brigada.png" width="30" height="30" alt="">
+                    <img src="/assets/img/logo_brigada_min.png" width="32" height="32" alt="">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
                     <span class="navbar-toggler-icon"></span>
