7aca62ee2fda2c70f3bfbac80c107868f2286a36
Nikita Tsapko <otghikogh@ya.ru>
2024-05-05 17:47:31 +0300

    developing

--- core/template/default.php
+++ core/template/default.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" type="text/css" href="/assets/css/jquery.datetimepicker.css">
     <link rel="stylesheet" type="text/css" href="/assets/css/main.css">
     <link rel="stylesheet" type="text/css" href="/assets/css/dashboard.css">
-    <title>$<?php echo ($title) ? $title : "Brigada MIIT"; ?></title>
+    <title><?php echo ($title) ? $title : "Brigada MIIT"; ?></title>
     <script src="/assets/js/jquery.min.js"></script>
     <!--<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>-->
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
