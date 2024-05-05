fc2b942d7464b5301786728bfd643b3fe057798b
Nikita Tsapko <otghikogh@ya.ru>
2024-05-05 18:45:15 +0300

    developing

--- core/template/uploads/files.php
+++ core/template/uploads/files.php
@@ -116,7 +116,7 @@
                                 die("Categories error");
                             for($i = 0; $i < $query->num_rows; $i++) {
                                 $results = $query->fetch_assoc();
-                                echo "<option value='".$results['id']."' label='".$results['name']."'".(($results['category'] == $i) ? 'selected' : '').">";
+                                echo "<option value='".$results['id']."' label='".$results['name']."'".(($result['category'] == $i) ? 'selected' : '').">";
                             }
                         ?>
                     </select><br>
