0a180c60e7d57b62cccb003cc17939f670adc04d
Nikita Tsapko <otghikogh@ya.ru>
2024-05-05 13:45:35 +0300

    developing

--- core/template/auth/login.php
+++ core/template/auth/login.php
@@ -1,19 +1,6 @@
 <div class="container" style="padding-top: 6%;">  
     <!--<p class="page-title">Настройки</p>-->
     <div class="form">
-        <!--<div class="col-12 offset-md-2 col-md-8 offset-lg-3 col-lg-6 offset-xl-4 col-xl-4">
-            <div class="login">
-                <div class="text-center">
-                    <h2 class="active">Вход</h2>
-                </div><br><br>
-                <input type="text" class="text" id="login">
-                <span>Логин</span><br><br>
-                <input type="password" class="text" id="password">
-                <span>Пароль</span><br>
-                <div class="res text-center"></div>
-                <button class="signin" onclick="login();">Войти</button>
-            </div>
-        </div>-->
         <div class="col-12">
             <div class="in">
                 <div class="text-center">
