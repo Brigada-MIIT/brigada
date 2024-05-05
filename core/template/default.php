57838defb4262bada4302239347e43e09a9516bc
Nikita Tsapko <otghikogh@ya.ru>
2024-05-05 13:13:41 +0300

    developing

--- core/template/default.php
+++ core/template/default.php
@@ -13,50 +13,52 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   </head>
   <body>
-    <header>
-        <nav class="navbar navbar-expand-md navbar-light bg-light">
-            <a class="navbar-brand" href="#">
-                <img src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="">
-             </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Главная <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Категории</a>
-                    </li>
-                </ul>
-                <ul class="navbar-nav">
-                    <?php if(!$system->auth()) echo '
-                    <li class="nav-item">
-                        <a class="nav-link" href="/app/auth">Логин</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/app/register">Регистрация</a>
-                    </li>';
-                    else echo '
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16" style="top: 0.13em;position: relative"><path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/><path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/></svg>
-                            '. $_user['surname'] .'
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink" style="left: -55%;">
-                            <a class="dropdown-item" href="/profile">Профиль</a>
-                            <a class="dropdown-item" href="/profile/uploads">Мои загрузки</a>
-                            <hr style="margin-top: 5px;margin-bottom: 4px;">
-                            <a class="dropdown-item" href="/logout">Выйти</a>
-                        </div>
-                    </li>'; ?>
-                </ul>
-            </div>
-        </nav>
-    </header>
-    <main>
-        <?php include $content;?>
-    </main>
+    <div class="wrapper">
+        <header>
+            <nav class="navbar navbar-expand-md navbar-light bg-light">
+                <a class="navbar-brand" href="#">
+                    <img src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Главная <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Категории</a>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav">
+                        <?php if(!$system->auth()) echo '
+                        <li class="nav-item">
+                            <a class="nav-link" href="/app/auth">Логин</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/app/register">Регистрация</a>
+                        </li>';
+                        else echo '
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16" style="top: 0.13em;position: relative"><path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/><path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/></svg>
+                                '. $_user['surname'] .'
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink" style="left: -55%;">
+                                <a class="dropdown-item" href="/profile">Профиль</a>
+                                <a class="dropdown-item" href="/profile/uploads">Мои загрузки</a>
+                                <hr style="margin-top: 5px;margin-bottom: 4px;">
+                                <a class="dropdown-item" href="/logout">Выйти</a>
+                            </div>
+                        </li>'; ?>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        <main>
+            <?php include $content;?>
+        </main>
+    </div>
   </body>
 </html>
