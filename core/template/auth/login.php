db0ea81274720e4ec9cdbe5b26235c459975935b
Nikita Tsapko <otghikogh@ya.ru>
2024-05-05 13:22:26 +0300

    developing

--- core/template/auth/login.php
+++ core/template/auth/login.php
@@ -1,10 +1,10 @@
 <div class="container justify-content-center">  
-    <p class="page-title">Настройки</p>
+    <!--<p class="page-title">Настройки</p>-->
     <div class="form">
         <div class="col-12">
             <h1 class="title"></h1>
         </div>
-        <div class="col-12 offset-md-2 col-md-8 offset-lg-3 col-lg-6 offset-xl-4 col-xl-4">
+        <!--<div class="col-12 offset-md-2 col-md-8 offset-lg-3 col-lg-6 offset-xl-4 col-xl-4">
             <div class="login">
                 <div class="text-center">
                     <h2 class="active">Вход</h2>
@@ -15,6 +15,19 @@
                 <span>Пароль</span><br>
                 <div class="res text-center"></div>
                 <button class="signin" onclick="login();">Войти</button>
+            </div>
+        </div>-->
+        <div class="col-12">
+            <div class="in">
+                <label for="login">Логин</label><br>
+                <input type="text" class="text" id="login">
+                <label for="password">Логин</label><br>
+                <input type="password" class="text" id="password">
+            </div>
+        </div>
+        <div class="col-12">
+            <div class="in">
+                <br><button type="submit" class="submit" onclick="login();">Войти</button>
             </div>
         </div>
     </div>
