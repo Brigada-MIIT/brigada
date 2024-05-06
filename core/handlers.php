b47bd466bcf886e6bf338cead9c0f4541b4ad37a
Nikita Tsapko <otghikogh@ya.ru>
2024-05-07 00:14:55 +0300

    developing

--- core/handlers.php
+++ core/handlers.php
@@ -384,8 +384,7 @@ function api_users_edit() {
     $surname = !empty($_POST['surname']) ? $_POST['surname'] : res(0, "surname error");
     $patronymic = !empty($_POST['patronymic']) ? $_POST['patronymic'] : res(0, "patronymic error");
 
-    res(0, "email_verifed: " . $email_verifed . ", ban_upload: " . $ban_upload . ", ban: " . $ban . "; email_verifed (user): " . $user['email_verifed']);
-
+    res(0, "UPDATE `users` SET `user_type` = '$role', `ban` = '$ban', `ban_upload` = '$ban_upload', `email_verifed` = '$email_verifed', `lastname` = '$lastname', `surname` = '$surname', `patronymic` = '$patronymic' WHERE `id` = '$user_id'");
     $query = $db->query("UPDATE `users` SET `user_type` = '$role', `ban` = '$ban', `ban_upload` = '$ban_upload', `email_verifed` = '$email_verifed', `lastname` = '$lastname', `surname` = '$surname', `patronymic` = '$patronymic' WHERE `id` = '$user_id'");
     if(!$query) res(0, "mysql error");
     res(1, "Данные пользователя успешно обновлены");
