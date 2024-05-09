c50f56bcbac8e25d7d0cd939664bbe6b86b0f1ac
Nikita <otghikogh@ya.ru>
2024-05-10 00:44:33 +0300

    developing

--- core/template/uploads/view.php
+++ core/template/uploads/view.php
@@ -1,17 +1,17 @@
 <div class="container mt-4">
     <div class="card">
         <div class="card-header">
-            Post Information
+            Информация о загрузке
         </div>
         <div class="card-body">
-            <h5 class="card-title">Name: {{post.name}}</h5>
-            <p class="card-text">Description: {{post.description}}</p>
-            <p class="card-text">Author: {{post.author}}</p>
-            <p class="card-text">Date: {{post.date}}</p>
-            <p class="card-text">Status: {{post.status}}</p>
-            <p class="card-text">Category: {{post.category}}</p>
+            <h5 class="card-title">Название: <?php echo $result['name'] ?></h5>
+            <p class="card-text">Описание: <?php echo $result['description'] ?></p>
+            <p class="card-text">Автор: <?php echo (empty($result_author['lastname']) ? "Пользователь удалён" : ($result_author['lastname']) . " " . $result_author['surname']) ?></p>
+            <p class="card-text">Дата: <?php echo unixDateToString($result['created']) ?></p>
+            <p class="card-text">Статус: <?php echo ($result['status'] != -1 ? (($result['status'] == 1) ? "Опубликован" : "Не опубликован") : "Скрыт") ?></p>
+            <p class="card-text">Категория: <?php $result_category['name']; ?></p>
             <hr>
-            <h5 class="card-title">Files:</h5>
+            <h5 class="card-title">Файлы:</h5>
             <ul class="list-group">
                 {{#post.files}}
                 <li class="list-group-item">
@@ -20,6 +20,7 @@
                         {{file_name}}
                     </a>
                 </li>
+                {{/post.files}}
             </ul>
         </div>
     </div>
