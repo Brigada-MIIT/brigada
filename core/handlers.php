27be30f0148a02751d5f98ea43e761e3179fb81e
Nikita <otghikogh@ya.ru>
2024-05-09 23:36:24 +0300

    developing

--- core/handlers.php
+++ core/handlers.php
@@ -278,7 +278,8 @@ function api_main_get_uploads() {
                 $username = "<a target='_blank' href='/profile/".$row['author']."'>".$user['surname']." ".$user['lastname']."</a>";
             }
             
-            $row['id'] = "<a href='/uploads/view/".$row['id']."'>".$row['id']."</a>";
+            $row['id'] = "<a target='_blank' href='/uploads/view/".$row['id']."'>".$row['id']."</a>";
+            $row['name'] = "<a target='_blank' href='/uploads/view/".$row['id']."'>".$row['name']."</a>";
             $row['created'] = unixDateToString(intval($row['created']));
             $row['author'] = $username;
             $data[] = $row;
