b29d3f7b709c4abf8a7f1114448936f482bb8cdf
Nikita <otghikogh@ya.ru>
2024-05-10 01:04:15 +0300

    developing

--- core/template/uploads/view.php
+++ core/template/uploads/view.php
@@ -17,16 +17,15 @@
                     $files = json_decode($result['files']);
                     $count = (!empty($files) ? count($files) : 0);
                     for($i = 0; $i < count($files); $i++) {
-                        echo "<br><a href='/uploads/files/download/".$files[$i]->id."' target='_blank'>".$files[$i]->name."</a>";
                         echo "
                             <li class='list-group-item'>
-                                <a href='/uploads/files/download/".$files[$i]->id."' target='_blank' class='btn btn-link d-flex justify-content-between align-items-center'>
-                                    <div>
+                                <div class='d-flex justify-content-between align-items-center'>
+                                    <a href='/uploads/files/download/".$files[$i]->id."' target='_blank' class='btn btn-link'>
                                         <!--<img src='microsoft_word_logo.png' alt='Microsoft Word' width='16' height='16' class='mr-1'>-->
                                         ".$files[$i]->name."
-                                    </div>
-                                </a>
-                                <span class='ml-auto'>{{file_size}}</span>
+                                    </a>
+                                    <span>".$files[$i]->size."</span>
+                                </div>
                             </li>
                         ";
                     }
