7bf6c6ec33160a58687f8c55206f68af324ab893
Nikita <otghikogh@ya.ru>
2024-05-10 02:28:32 +0300

    developing

--- core/template/main.php
+++ core/template/main.php
@@ -2,7 +2,7 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap4.min.js"></script>
-<!--<script src="https://cdn.datatables.net/colreorder/1.5.6/js/dataTables.colReorder.min.js"></script>-->
+<script src="https://cdn.datatables.net/colreorder/1.5.6/js/dataTables.colReorder.min.js"></script>
 <style>
     .row {
         overflow-x: auto;
