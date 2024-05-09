43e4439fbefc937bea20d2894e1ada874910978d
Nikita <otghikogh@ya.ru>
2024-05-10 02:36:57 +0300

    developing

--- core/template/uploads/view.php
+++ core/template/uploads/view.php
@@ -8,7 +8,7 @@
             <p class="card-text"><b>Описание:</b> <?php echo $result['description'] ?></p>
             <p class="card-text"><b>Автор:</b> <?php echo (empty($result_author['lastname']) ? "Пользователь удалён" : "<a target='_blank' href='/profile/".$result_author['id']."'>".($result_author['surname'])." ".$result_author['lastname'])."</a>" ?></p>
             <p class="card-text"><b>Дата:</b> <?php echo unixDateToString(intval($result['created'])) ?></p>
-            <p class="card-text"><b>Статус:</b> <?php echo ($result['status'] != -1 ? (($result['status'] == 1) ? "Опубликован" : "Не опубликован") : "Скрыт") ?></p>
+            <p class="card-text"><b>Статус:</b> <?php echo ($result['status'] != -1 ? (($result['status'] == 1) ? "Опубликован" : "Не опубликован") : "Скрыто администратором") ?></p>
             <p class="card-text"><b>Категория:</b> <?php echo $result_category['name']; ?></p>
             <hr>
             <h5 class="card-title"><b>Файлы:</b></h5>
@@ -31,7 +31,8 @@
                     }
                 ?>
             </ul>
-            <a href="#" class="btn btn-primary float-right mr-2">Редактировать пост</a>
+            <?php if(!$check) echo "
+                <a href='/uploads/edit/".$result['id']."' class='btn btn-primary float-right mr-2' style='margin-top: 20px;'>Редактировать пост</a>"; ?>
         </div>
     </div>
 </div>
