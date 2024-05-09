e1e0cba07a78af163bcf919fc8416aa65cc51e48
Nikita <otghikogh@ya.ru>
2024-05-09 22:48:29 +0300

    developing

--- public_html/index.php
+++ public_html/index.php
@@ -3,6 +3,8 @@ define('TIMEZONE', 'Europe/Moscow');
 date_default_timezone_set(TIMEZONE);
 header('Content-Type: text/html; charset=utf-8');
 
+setlocale(LC_ALL, "ru_RU");
+
 define('KB', 1024);
 define('MB', 1048576);
 define('GB', 1073741824);
