0314f3f58796b9db14d6888888eaf78992be2068
Nikita <otghikogh@ya.ru>
2024-05-10 00:46:53 +0300

    developing

--- core/template/uploads/view.php
+++ core/template/uploads/view.php
@@ -6,7 +6,7 @@
         <div class="card-body">
             <h5 class="card-title">Название: <?php echo $result['name'] ?></h5>
             <p class="card-text">Описание: <?php echo $result['description'] ?></p>
-            <p class="card-text">Автор: <?php echo (empty($result_author['lastname']) ? "Пользователь удалён" : ($result_author['ыгктфьу']) . " " . $result_author['lastname']) ?></p>
+            <p class="card-text">Автор: <?php echo (empty($result_author['lastname']) ? "Пользователь удалён" : ($result_author['surname']) . " " . $result_author['lastname']) ?></p>
             <p class="card-text">Дата: <?php echo unixDateToString(intval($result['created'])) ?></p>
             <p class="card-text">Статус: <?php echo ($result['status'] != -1 ? (($result['status'] == 1) ? "Опубликован" : "Не опубликован") : "Скрыт") ?></p>
             <p class="card-text">Категория: <?php echo $result_category['name']; ?></p>
