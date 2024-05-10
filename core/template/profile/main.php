e4e2b7e46cceb78eda4e6792a530d82f34cd720c
Nikita <otghikogh@ya.ru>
2024-05-10 14:58:43 +0300

    developing

---
+++ core/template/profile/main.php
@@ -0,0 +1,44 @@
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-6">
+            <h2>Профиль пользователя</h2>
+            <hr>
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Имя пользователя (ФИО)</h5>
+                    <p class="card-text">Роль пользователя</p>
+                    <img src="avatar.jpg" class="img-fluid rounded-circle" alt="Avatar">
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <h2>Загрузки пользователя</h2>
+            <hr>
+            <table id="uploadsTable" class="table table-striped table-bordered" style="width:100%">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Название</th>
+                        <th>Дата</th>
+                        <th>Статус</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Название файла 1</td>
+                        <td>01.01.2022</td>
+                        <td>Опубликован</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Название файла 2</td>
+                        <td>02.01.2022</td>
+                        <td>Не опубликован</td>
+                    </tr>
+            <!-- Добавьте больше строк, если нужно -->
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
