b1989f1a13ae56449fb1e864d7c275505bd05422
Nikita <otghikogh@ya.ru>
2024-05-10 15:31:13 +0300

    developing

--- core/template/profile/main.php
+++ core/template/profile/main.php
@@ -1,42 +1,31 @@
 <div class="container mt-5">
-    <div class="row">
-      <div class="col-md-4">
-        <div class="card">
-          <img src="avatar.jpg" class="card-img-top" alt="Avatar">
-          <div class="card-body">
-            <h5 class="card-title">Имя пользователя (ФИО)</h5>
-            <p class="card-text">Роль пользователя</p>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-8">
-        <h2>Загрузки пользователя</h2>
-        <hr>
-        <table id="uploadsTable" class="table table-striped table-bordered" style="width:100%">
+    <div class="card">
+      <div class="card-body">
+        <h2 class="card-title">Имя Фамилия</h2>
+        <p class="card-text">Отчество</p>
+        <img src="avatar.jpg" class="img-fluid rounded-circle my-3" alt="Avatar">
+        <p><strong>Роль:</strong> Администратор</p>
+        <table id="downloadsTable" class="table table-striped table-bordered" style="width:100%">
           <thead>
             <tr>
-              <th>ID</th>
-              <th>Название</th>
-              <th>Дата</th>
-              <th>Статус</th>
+              <th>Файл</th>
+              <th>Размер</th>
+              <th>Дата загрузки</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td>1</td>
-              <td>Название файла 1</td>
-              <td>01.01.2022</td>
-              <td>Опубликован</td>
+              <td>File 1</td>
+              <td>10 MB</td>
+              <td>2021-01-01</td>
             </tr>
             <tr>
-              <td>2</td>
-              <td>Название файла 2</td>
-              <td>02.01.2022</td>
-              <td>Не опубликован</td>
+              <td>File 2</td>
+              <td>5 MB</td>
+              <td>2021-02-15</td>
             </tr>
-            <!-- Добавьте больше строк, если нужно -->
           </tbody>
         </table>
       </div>
     </div>
-  </div>
+</div>
