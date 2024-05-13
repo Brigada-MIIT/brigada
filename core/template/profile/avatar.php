40c0a3eb2db4f0ad5d7ad592351211cf0ca23465
Nikita Tsapko <otghikogh@ya.ru>
2024-05-14 00:26:04 +0300

    developing

--- core/template/profile/avatar.php
+++ core/template/profile/avatar.php
@@ -7,7 +7,7 @@
                     <br><input type="file" name="avatar"><br><br>
                     <div class="col-12" style="margin-top: 5%;">
                         <div class="in">
-                            <button type="submit" class="submit mr-4 mb-2">Сохранить</button>
+                            <button id="submit" type="submit" class="submit mr-4 mb-2">Сохранить</button>
                         </div>
                     </div>
                 </form>
@@ -16,8 +16,8 @@
     </div>
     <div class="col-12" style="margin-top: 5%;">
         <div class="in">
-            <div class="btn-group d-flex flex-wrap align-items-center">
-                <button class="submit mr-4 mb-2">Удалить аватар</button>
+            <div class="btn-group d-flex flex-wrap">
+                <button class="submit mr-4 mb-2" style="margin-right: 10px;">Удалить аватар</button>
             </div>
         </div>
     </div>
