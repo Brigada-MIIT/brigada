7c7a4995682dfebee6f43bc8e70448d30f409776
Nikita Tsapko <otghikogh@ya.ru>
2024-05-15 01:17:49 +0300

    developing

--- core/template/users/edit.php
+++ core/template/users/edit.php
@@ -127,8 +127,8 @@
                             <p><b>Дата регистрации:</b> <?php echo unixDateToString(intval($user['registred'])) ?></p>
                             <p><b>Подтверждение Email:</b> <?php echo ( ($user['email_verifed'] == 1) ? "Да" : "Нет" ) ?></p>
                             <p><b>Статус 2FA:</b> <?php echo ( !empty($user['2fa_secret']) ? "Включено" : "Выключено" ) ?></p>
-                            <?php echo ( !empty($user['ban']) ? "<p><b>Блокировка:</b> Да (<a href='/profile/'".$user['ban'].">кем выдана?</a>)</p>" : "" ) ?>
-                            <?php echo ( !empty($user['ban_upload']) ? "<p><b>Блокировка загрузки файлов:</b> Да (<a href='/profile/'".$user['ban_upload'].">кем выдана?</a>)</p>" : "" ) ?>
+                            <?php echo ( !empty($user['ban']) ? "<p><b>Блокировка:</b> Да (<a href='/profile/".$user['ban']."'>кем выдана?</a>)</p>" : "" ) ?>
+                            <?php echo ( !empty($user['ban_upload']) ? "<p><b>Блокировка загрузки файлов:</b> Да (<a href='/profile/".$user['ban_upload']."'>кем выдана?</a>)</p>" : "" ) ?>
                         </div>
                     </div>
                     <div class="d-flex flex-wrap">
