a701fb887d4df589f19353f8af4a0e4d7315bb79
Nikita Tsapko <otghikogh@ya.ru>
2024-05-14 22:25:52 +0300

    developing

--- core/handlers.php
+++ core/handlers.php
@@ -540,8 +540,8 @@ function api_users_get_users() {
         while($row = $query->fetch_assoc()) {
             $row['email'] = "<a style='color: inherit' target='_blank' href='/app/users/".$row['id']."/edit' title='".$row['lastname']." ".$row['surname']."".((!empty($row['patronymic'])) ? " ".$row['patronymic'] : "")."'>".$row['email']."</a>";
             $row['registred'] = "<a style='color: inherit' target='_blank' href='/app/users/".$row['id']."/edit'>".unixDateToString(intval($row['registred']))."</a>";
+            $row['user_type'] = "<a style='color: inherit' target='_blank' href='/app/users/".$row['id']."/edit'>".$system->getNameRole($row['user_type'])."</a>";
             $row['id'] = "<a target='_blank' href='/app/users/".$row['id']."/edit'>".$row['id']."</a>";
-            $row['user_type'] = "<a target='_blank' href='/app/users/".$row['id']."/edit'>".$row['id']."</a>";
             $data[] = $row;
         }
     }
