07222d63edd6ece2aea1db0db0bb42b0d8c2c6e1
Nikita Tsapko <otghikogh@ya.ru>
2024-05-20 23:26:30 +0300

    cleaning

--- core/config.php
+++ core/config.php
@@ -1,22 +1,12 @@
 <?php
-define('db_host', 'localhost');
-define('db_user', 'brigada'); 
-define('db_basename', 'brigada');
+define('db_host', '${{ secrets.DATABASE_HOST }}');
+define('db_user', '${{ secrets.DATABASE_USERNAME }}'); 
+define('db_basename', '${{ secrets.DATABASE_BASENAME }}');
 define('db_password', 's194GJ!18xMrTaq9^');
-define('secret_key', 'KaX81mRRtEu26j_40__49');
 
 define('mysql_port', '3306');
 
 ini_set("allow_url_fopen", 1);
 date_default_timezone_set('Europe/Moscow');
 
-$roles = array(
-	1 => "Test",
-);
-
-$permissions_for_roles = array(
-	1 => array(
-		"DASHBOARD" => TRUE,
-	)
-);
 ?>
--- public_html/assets/js/reloader-online.js
+++
@@ -1,9 +0,0 @@
-$(document).ready (function () {
-    var updater = setInterval (function () {
-        $('div#server-online').load ('OnlineStatus.php', 'update=true');
-    }, 1000);
-
-    var updaterr = setInterval (function () {
-        $('div#server-ping').load ('PingStatus.php', 'update=true');
-    }, 1000);
-});
