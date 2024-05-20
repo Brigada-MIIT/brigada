90b06c40a73ccf402e2db1e728c2565dfef0b49f
Nikita Tsapko <otghikogh@ya.ru>
2024-05-20 23:56:31 +0300

    developing

--- core/config.php
+++ core/config.php
@@ -1,4 +1,5 @@
 <?php
+
 define('db_host', $_ENV['DATABASE_HOST']);
 define('db_user', $_ENV['DATABASE_USERNAME']); 
 define('db_basename', $_ENV['DATABASE_BASENAME']);
@@ -6,7 +7,7 @@ define('db_password', 's194GJ!18xMrTaq9^');
 
 define('mysql_port', '3306');
 
-ini_set("allow_url_fopen", 1);
+ini_set('allow_url_fopen', 1);
 date_default_timezone_set('Europe/Moscow');
 
 ?>
