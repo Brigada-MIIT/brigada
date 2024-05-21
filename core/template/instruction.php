d9806ccdf87255ed57dd1f373de9333556f93c54
Nikita Tsapko <otghikogh@ya.ru>
2024-05-21 17:17:55 +0300

    developing

--- core/template/instruction.php
+++ core/template/instruction.php
@@ -28,7 +28,7 @@
             <div class="card">
                 <div class="card-body">
                     <a href="https://www.construct.net/en/free-online-games/oeh-ea-64589/play"><h2 class="card-title"><strong>Уровень 1</strong></h2></a>
-                    <p class="card-text">Описание туториала 1. Этот туториал поможет вам узнать больше о ...</p>
+                    <p class="card-text">В этом уроке Вы узнаете, как авторизоваться на нашем сайте</p>
                     <div>
                         <a target="_blank" href="https://www.construct.net/en/free-online-games/oeh-ea-64589/play"><img src="/assets/img/qr-code-level-1.png" class="qr-code" alt="Уровень 1"></a>
                     </div>
@@ -39,7 +39,7 @@
             <div class="card">
                 <div class="card-body">
                     <a href="https://www.construct.net/en/free-online-games/oeh-ea-64593/play"><h2 class="card-title"><strong>Уровень 2</strong></h2></a>
-                    <p class="card-text">Описание туториала 2. Этот туториал поможет вам узнать больше о ...</p>
+                    <p class="card-text">В этом уроке Вы можете узнаете, как пользоваться главной страницей и какой у нее функционал</p>
                     <div>
                         <a target="_blank" href="https://www.construct.net/en/free-online-games/oeh-ea-64593/play"><img src="/assets/img/qr-code-level-2.png" class="qr-code" alt="Уровень 2"></a>
                     </div>
@@ -50,7 +50,7 @@
             <div class="card">
                 <div class="card-body">
                     <a href="https://www.construct.net/en/free-online-games/oeh-ea-64594/play"><h2 class="card-title"><strong>Уровень 3</strong></h2></a>
-                    <p class="card-text">Описание туториала 3. Этот туториал поможет вам узнать больше о ...</p>
+                    <p class="card-text">В этом уроке Вы узнаете, как создавать свой пост, который может включать в себе несколько файлов</p>
                     <div>
                         <a target="_blank" href="https://www.construct.net/en/free-online-games/oeh-ea-64594/play"><img src="/assets/img/qr-code-level-3.png" class="qr-code" alt="Уровень 3"></a>
                     </div>
@@ -61,7 +61,7 @@
             <div class="card">
                 <div class="card-body">
                     <a href="https://www.construct.net/en/free-online-games/oeh-ea-64595/play"><h2 class="card-title"><strong>Уровень 4</strong></h2></a>
-                    <p class="card-text">Описание туториала 4. Этот туториал поможет вам узнать больше о ...</p>
+                    <p class="card-text">В этом уроке Вы узнаете о сайте больше. В нём мы расскажем про вещи, о которых не упоминали раньше</p>
                     <div>
                         <a target="_blank" href="https://www.construct.net/en/free-online-games/oeh-ea-64595/play"><img src="/assets/img/qr-code-level-4.png" class="qr-code" alt="Уровень 4"></a>
                     </div>
