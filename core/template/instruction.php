2c6b0751d1aa8e371905dc7b02bbec60a146d700
Nikita Tsapko <otghikogh@ya.ru>
2024-05-21 13:54:09 +0300

    developing

--- core/template/instruction.php
+++ core/template/instruction.php
@@ -28,10 +28,10 @@
         <div class="col-md-6 tutorial-card">
             <div class="card">
                 <div class="card-body">
-                    <a href="https://www.construct.net/en/free-online-games/oeh-ea-64589/play"><b><h2 class="card-title">Уровень 1</h2></b></a>
+                    <a href="https://www.construct.net/en/free-online-games/oeh-ea-64589/play"><h2 class="card-title"><strong>Уровень 1</strong></h2></a>
                     <p class="card-text">Описание туториала 1. Этот туториал поможет вам узнать больше о ...</p>
                     <div>
-                        <a href="https://www.construct.net/en/free-online-games/oeh-ea-64589/play"><img src="/assets/img/qr-code-level-1.png" class="qr-code" alt="Уровень 1"></a>
+                        <a target="_blank" href="https://www.construct.net/en/free-online-games/oeh-ea-64589/play"><img src="/assets/img/qr-code-level-1.png" class="qr-code" alt="Уровень 1"></a>
                     </div>
                 </div>
             </div>
@@ -39,10 +39,10 @@
         <div class="col-md-6 tutorial-card">
             <div class="card">
                 <div class="card-body">
-                    <a href="https://www.construct.net/en/free-online-games/oeh-ea-64593/play"><b><h2 class="card-title">Уровень 2</h2></b></a>
+                    <a href="https://www.construct.net/en/free-online-games/oeh-ea-64593/play"><h2 class="card-title">Уровень 2</h2></a>
                     <p class="card-text">Описание туториала 2. Этот туториал поможет вам узнать больше о ...</p>
                     <div>
-                        <a href="https://www.construct.net/en/free-online-games/oeh-ea-64593/play"><img src="/assets/img/qr-code-level-2.png" class="qr-code" alt="Уровень 2"></a>
+                        <a target="_blank" href="https://www.construct.net/en/free-online-games/oeh-ea-64593/play"><img src="/assets/img/qr-code-level-2.png" class="qr-code" alt="Уровень 2"></a>
                     </div>
                 </div>
             </div>
@@ -50,10 +50,10 @@
         <div class="col-md-6 tutorial-card">
             <div class="card">
                 <div class="card-body">
-                    <a href="https://www.construct.net/en/free-online-games/oeh-ea-64594/play"><b><h2 class="card-title">Уровень 3</h2></b></a>
+                    <a href="https://www.construct.net/en/free-online-games/oeh-ea-64594/play"><h2 class="card-title">Уровень 3</h2></a>
                     <p class="card-text">Описание туториала 3. Этот туториал поможет вам узнать больше о ...</p>
                     <div>
-                        <a href="https://www.construct.net/en/free-online-games/oeh-ea-64594/play"><img src="/assets/img/qr-code-level-3.png" class="qr-code" alt="Уровень 3"></a>
+                        <a target="_blank" href="https://www.construct.net/en/free-online-games/oeh-ea-64594/play"><img src="/assets/img/qr-code-level-3.png" class="qr-code" alt="Уровень 3"></a>
                     </div>
                 </div>
             </div>
@@ -61,10 +61,10 @@
         <div class="col-md-6 tutorial-card">
             <div class="card">
                 <div class="card-body">
-                    <a href="https://www.construct.net/en/free-online-games/oeh-ea-64595/play"><b><h2 class="card-title">Уровень 4</h2></b></a>
+                    <a href="https://www.construct.net/en/free-online-games/oeh-ea-64595/play"><h2 class="card-title">Уровень 4</h2></a>
                     <p class="card-text">Описание туториала 4. Этот туториал поможет вам узнать больше о ...</p>
                     <div>
-                        <a href="https://www.construct.net/en/free-online-games/oeh-ea-64595/play"><img src="/assets/img/qr-code-level-4.png" class="qr-code" alt="Уровень 4"></a>
+                        <a target="_blank" href="https://www.construct.net/en/free-online-games/oeh-ea-64595/play"><img src="/assets/img/qr-code-level-4.png" class="qr-code" alt="Уровень 4"></a>
                     </div>
                 </div>
             </div>
